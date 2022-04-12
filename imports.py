bac15c8ac2d4408aa681b8acc9e118f55886325b
Mistayan <Helixmastaz@gmail.com>
2022-04-12 15:02:11 +0200

    Global Imports

--- imports.py
+++ imports.py
@@ -1,6 +1,14 @@
-import Generators.Generateurs as Gen
-import Verifications.Base as Secu
-from string import digits as DIGITS
-from string import hexdigits as HEXADECIMAL
-from Verifications import ErrorsHandling
+#!-- coding:latin-1 --!#
 
+if __name__ != '__main__':
+    from Generators import Generateurs as Gen
+    from Verifications import Securite as Secu
+    from Comptes.Compte import Compte
+    from Comptes.CompteEpargne import CompteEpargne
+    from Comptes.CompteCourant import CompteCourant
+    import json
+    from Verifications.ErrorsHandling import UndefinedUserError
+    from Messages import Static_strings as Message
+
+if __name__ == '__main__':
+    print(EXECUTE)
