59c45e1335e3b2a0cffa4eee8c2ba3d42c282105
Mistayan <Helixmastaz@gmail.com>
2022-04-12 15:03:21 +0200

    Fonctions de base de vérifications diverses

--- Verifications/Securite.py
+++ Verifications/Securite.py
@@ -3,27 +3,42 @@
 ##########################################  IMPORTS  #####################################################
 import Generators.Generateurs as Gen
 
+
 ##########################################  Definition classe  #####################################################
 
 
 class Verif:  # Définition d'une classe pour future maitrise de la sécurité des outils de vérifications
-    def dispo(self=None, compte: str = None):
+    """
+    Possède plusieurs fonctions de vérification
+    dispo: vérifier la disponibilité d'un compte
+    ... d'autres à venir
+    """
+    def dispo(self=None, compte: str = None, liste=None): #Annotation très étrange pour rendre statique... pas beau !
         """
-            Vérifie la disponibilité d'un numéro de compte (PKID)
+            Vérifie la disponibilité d'un numéro de compte (UUID)
 
              Si le numéro n'est pas au format valide, retourne False
 
-             Si le compte est trouvé, retourne le compte
+             Si le compte est trouvé, retourne le compte sous forme 'compte':'num_cpt'
 
              Si le compte n'existe pas, retourne True
         """
+        # TODO si json pas accepté: 'nom : numero : solde : monnaie : type : arg1 : arg2''
         if not compte or not compte.isdigit() or not len(compte) == 10:
             return False
         else:
-            f = Gen.my_open("comptes.txt")
-            for line in f.readline():
-                if compte in line:
-                    return line.split(":")[0]  # Ne récupérer que la première partie
+            if not liste:  # En dehors du cadre de l'application
+                f = Gen.my_open("comptes.json", 'r+')
+                if not f:
+                    return False
+                for line in f:
+                    if compte in line:
+                        f.close()
+                        return line.split(":")[0]  # Ne retourne que le numéro de compte...
+                f.close()
+            else:  # On a reçu une liste
+                pass  # TODO faire en liste, pour l'application
+
         return True
 
 ##########################################  Fonctions Partagées  ###################################################
