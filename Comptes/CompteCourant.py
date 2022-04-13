8d0e7aa6680eb2cd42c86d9dabe91dde0ec42fad
Mistayan <Helixmastaz@gmail.com>
2022-04-13 16:35:26 +0200

    Modification des imports Correction de la méthode appliquer_agios rm prints commentaires revus

--- Comptes/CompteCourant.py
+++ Comptes/CompteCourant.py
@@ -1,8 +1,12 @@
 #!-- coding:latin-1 --!#
 ##########################################  IMPORTS  ####################################################
 
-from imports import *
-import Messages.Static_strings as Message
+import Message.Static_strings as Message
+from Comptes.Compte import Compte
+from Generateurs import Generateur as Gen
+from Verifications import Securite as Secu
+from Verifications import ErreurUtilisateurNonDefini
+
 
 ##########################################  Definition Classe  ####################################################
 #
@@ -15,7 +19,7 @@ class CompteCourant(Compte):
         ARGUMENTS :
          autorisation : int/float
          interet_debiteur : int/float
-         agios : int
+         agios : int (12, pour 12% par exemple)
     """
 
     _autorisation_decouvert: float
@@ -24,19 +28,22 @@ class CompteCourant(Compte):
 
     #
     ##########################################  __init__  ####################################################
-    def __init__(self, autorisation: float = 50, agios: float = 12, **extra):
+    def __init__(self, autorisation: float = 50, agios: float = 4.73, **extra):
         if hasattr(extra, "nom"):
             nom = extra.get("nom")
             if nom == "" or nom == "Anonymous":
-                raise UndefinedUserError(self)
+                raise ErreurUtilisateurNonDefini(self)
 
         super().__init__(**extra)
         self._autorisation_decouvert = autorisation if autorisation >= 0 else 0
-        self._pourcentage_agios = agios / 100 if agios > 5 else 5
 
-        print(f"Ce compte est de type CompteCourant, avec autorisation de découvert de"
-              f" {self._autorisation_decouvert}{self.monnaie}, "
-              f"et un taux d'intérêts de {self._pourcentage_agios}%")
+        if agios >= 50 or agios <= 5:
+            agios = 5
+        self._pourcentage_agios = agios / 100
+
+        # print(f"Ce compte est de type CompteCourant, avec autorisation de découvert de"
+        #      f" {self._autorisation_decouvert}{self.monnaie}, "
+        #      f"et un taux d'intérêts de {self._pourcentage_agios}%")
 
     #
     ##########################################  Fonctions Normale  ####################################################
@@ -50,48 +57,47 @@ class CompteCourant(Compte):
 
         super().retrait(valeur, autorisation=self._autorisation_decouvert)
         #  TODO : pour etre plus gentil avec nos clients, on pourrait rajouter un if ? *wink* (et donc changer docs)
-        self.appliquer_agios(self._solde)
+        self.appliquer_agios()
 
-    def appliquer_agios(self, valeur: float):
+    def appliquer_agios(self):
         """
-           Si l'utilisateur effectue un retrait lors du découvert.... ça pique !
+           Si l'utilisateur effectue un retrait lors du découvert.... on appliquera cette méthode !
             L'on considèrera qu'à 0 euro, nous sommes déjà à découvert, n'ayant pas les capacités de retrait suffisantes
                On lui évitera cependant les frais de retraits lors du premier évènement qui l'aura mis en négatif
                (une seule fois)
             Attention ! Hors 'coup de pouce', CHAQUE opération à-découvert appliquera des frais. [Sujet de l'exercice]
         """
-        # FIXME : Mauvaise valeur de retour
         if self._solde > 0:
-            return print(Message.POSITIF) if Message.DEBUG else None
-        if self._solde < 0 and self.__coup_de_pouce is True:  # En négatif et pas encore utilisé le coup de pouce ?
+            pass
+        elif self._solde < 0 and self.__coup_de_pouce is True:  # En négatif et pas encore utilisé le coup de pouce ?
             self.__coup_de_pouce = False
-            return print(Message.COUP_DE_POUCE)
-
-        # Nous avons donc un dépensier .... Il va payer !
-        facturation = valeur * (self._pourcentage_agios / 100)
-        print(f"Votre compte va être débité de {facturation}{self.monnaie},"
-              f" dans le cadre de notre politique de retraits.")
-        self._solde = self._solde + facturation
-        self.afficher_solde()
+        else:
+            # Nous avons donc un dépensier .... Il va payer !
+            facturation = abs(self._solde * (self._pourcentage_agios / 100))
+            # print(f"Votre compte va être débité de {facturation}{self.monnaie},"
+            #       f" dans le cadre de notre politique de retraits.")
+            self._solde = self._solde - facturation
+            self.afficher_solde()
+        return self._solde
 
     #
     ##########################################  Fonctions Magiques  ####################################################
 
     def __str__(self) -> str:  # Surcharge
         """
-        Permet de renvoyer les informations du compte au format json
+        Permet de renvoyer les informations du compte au format str(json)
         """
-        # Mais on appelle quand même maman pour la prévenir qu'on reste chez Salomé ce soir;)
+        # Mais on appelle quand même maman pour récupérer ses informations.
         infos_parent = super().__str__()
-        infos_enfant = f"\"autorisation\":\"{self._autorisation_decouvert}\",\n" \
-                       f"\"interets\":\"{self._pourcentage_agios}\",\n" \
-                       f"\"pouce\":\"{self.__coup_de_pouce}\")\n" \
+        infos_enfant = f"\"autorisation\":\"{self._autorisation_decouvert}\"," \
+                       f"\"interets\":\"{self._pourcentage_agios}\"," \
+                       f"\"pouce\":\"{self.__coup_de_pouce}\"" \
                        "}"
         return infos_parent + infos_enfant
+
 
 ##########################################  Fonction test_module  ####################################################
 
 
 if __name__ == '__main__':
     print(Message.EXECUTE)
-
