a01abe2ee49a1ae32b644e242877d74feb80513c
Mistayan <Helixmastaz@gmail.com>
2022-04-13 16:41:54 +0200

    Création d'un début de questionnaire dynamique pour renseigner une classe dynamiquement (pas eu le temps... désolé)

---
+++ Generateurs/Questionnaire.py
@@ -0,0 +1,31 @@
+# from abc import ABC  # Abstractors
+import re
+
+import Message
+from Comptes import Compte, CompteCourant, CompteEpargne
+
+
+##########################################  Classe  #####################################################
+
+
+class Champ(object):
+    def __init__(self, *args):
+        self._arg = args
+
+
+class Questionnaire(Compte):
+    def __init__(self):
+        self._champs = None
+
+    def champs(self, type_compte: any):
+        if type(type_compte) is not CompteCourant or type(type_compte) is not CompteEpargne:
+            raise ValueError("Questionnaire.champs ne prends que des comptes bancaires")
+        if not self._champs:
+            self._champs = set()
+        for attribut in dir(type_compte):
+            valeur = getattr(type_compte, attribut)
+            self._champs.insert(attribut, valeur)
+
+
+if __name__ == '__main__':
+    print(Message.EXECUTE)
