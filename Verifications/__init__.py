7846667ee2809c6afe8ccb3e9bc95fcaffac0258
Mistayan <helixmastaz@gmail.com>
2022-04-24 20:39:32 +0200

    V 0.6.0
    Ajout de la fonction verif_format

--- Verifications/__init__.py
+++ Verifications/__init__.py
@@ -1,4 +1,5 @@
 # __init__.Verifications
 
 from .ErrorsHandling import ErreurUtilisateurNonDefini, ErreurSolde
-from .Securite import dispo, scan_file
+from .Securite import dispo, scan_file, verif_format
+
