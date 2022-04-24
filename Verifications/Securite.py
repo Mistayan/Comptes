b3be5e7fe6d0241a93f9d8d758039ffd9f8623cf
Mistayan <helixmastaz@gmail.com>
2022-04-24 20:38:27 +0200

    V 0.6.0
    Ajout de la fonction verif_format;
    Celle-ci permet de verifier l'integrite des donnees fournies dans comptes.json

--- Verifications/Securite.py
+++ Verifications/Securite.py
@@ -9,13 +9,13 @@ from json import JSONDecodeError
 ##########################################  Definition classe  #####################################################
 
 
-def dispo(compte: str = None, code: str = None):
+def dispo(compte: str = None, code: str = None, ):
     """
-        Vérifie la disponibilité d'un numéro de compte (UID)
+        Vérifie la disponibilite d'un numero de compte (UID)
 
-         Si le numéro n'est pas au format valide, retourne False
+         Si le numero n'est pas au format valide, retourne False
 
-         Si le compte est trouvé, retourne le compte sous forme False
+         Si le compte est trouve, retourne le compte sous forme False
 
              Si le compte n'existe pas, retourne True
     """
@@ -29,7 +29,7 @@ def dispo(compte: str = None, code: str = None):
         for line in f:
             if compte in line:
                 f.close()
-                return line.split(":")[0]  # Ne retourne que le numéro de compte...
+                return line.split(":")[1]  # Retourne le numero du compte.
         f.close()
 
 
@@ -52,6 +52,38 @@ def scan_file(numero_en_str: str = None):
             return True
     f.close()
     return False
+
+
+def verif_format(dict_compte):
+    """
+    arg: un dictionnaire de comptes
+    Verifie l'intégrité du compte récupéré dans un fichier techniquement accessible à l'utilisateur.
+
+    Retourne un compte du type voulu si les données sont valides, sinon False.
+    """
+    champs_compte = ["nom", "type_compte", "solde", "num_compte", "code", "monnaie"]
+    champs_compte_courant = ["autorisation", "agios"]
+    champs_compte_epargne = ["interets"]
+
+    if not type(dict_compte) is dict:
+        return False
+    for c in champs_compte:
+        if c not in dict_compte:
+            return False  # Un des champs n'existe pas.
+
+    if dict_compte["type_compte"] == "Epargne":
+        for c in champs_compte_epargne:
+            if c not in dict_compte:
+                return False  # Un des champs n'existe pas.
+        return True  # Les champs sont 'valides'
+
+    elif dict_compte["type_compte"] == "Courant":
+        for c in champs_compte_courant:
+            if c not in dict_compte:
+                return False  # Un des champs n'existe pas.
+        return True  # Les champs sont 'valides'
+
+    return False  # Les champs sont présents ET valides
 
 ##########################################  Fonctions Partagées  ###################################################
 
