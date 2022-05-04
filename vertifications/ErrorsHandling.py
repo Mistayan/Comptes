6ca63fa45f131904d1f68c3faf2442ce422d83c1
Mistayan <helixmastaz@gmail.com>
2022-05-04 13:41:28 +0200

    Ajout du HEADER, correction encoding. correction accents

---
+++ vertifications/ErrorsHandling.py
@@ -0,0 +1,13 @@
+#! coding:utf-8
+"""
+Auteur: Misatayan
+Projet : Compte-Bancaire
+"""
+class ErreurUtilisateurNonDefini(Exception):
+    # print("Impossible de creer un compte courant en tout anonymat... Vous devriez le savoir")
+    pass
+
+
+class ErreurSolde(Exception):
+    # print("Solde Insuffisant")
+    pass
