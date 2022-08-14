32de7520399b8185d0957fd496cc9bcf799d4992
Mistayan <helixmastaz@gmail.com>
2022-08-14 08:36:31 +0200

    minor changes for more accurate exchanges front/back usage of Gen.Encrypt (friendly interface to use encryptions) for demos only
    preview for mongo_db

--- comptes/__init__.py
+++ comptes/__init__.py
@@ -4,6 +4,6 @@ Init des packages de compte
 Editeur: Mistayan
 Projet: Comptes-Bancaires
 """
-from .Compte import Compte
-from .CompteCourant import CompteCourant
-from .CompteEpargne import CompteEpargne
+from .compte import Compte
+from .compte_courant import CompteCourant
+from .compte_epargne import CompteEpargne
