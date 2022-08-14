5e43f9e2f4178d2d8e2464fdcbad1fe7bc5baa05
Mistayan <helixmastaz@gmail.com>
2022-08-14 08:37:34 +0200

    minor rework

--- Generateurs/encryption.py
+++ Generateurs/encryption.py
@@ -13,8 +13,7 @@ class Encrypt:
 
     def __init__(self, to_crypt):
         self.to_encrypt = to_crypt
-        self.vv = vars()
-        print(f"demo helper: {self.vv} => {self.__str__()}")
+        print(f"demo crypt-helper: {to_crypt} => {self.__str__()}")
 
     def __str__(self):
         return md5(str(self.to_encrypt).encode("utf-8")).hexdigest()
